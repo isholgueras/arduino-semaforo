e8e4cb90f02b7e806055dfb306b470a3db034fa6
Ignacio Sánchez <chet161984@gmail.com>
2016-01-16 11:25:31 +0100

    Timer entre luces funcionando

---
+++ arduino-semaforo.ino
@@ -0,0 +1,54 @@
+
+int redLightPin = 5;
+int yellowLightPin = 6;
+int greenLightPin = 7;
+
+int redTime = 10000;
+int yellowTime = 4000;
+int yellowTimeInterval = 500;
+int greenTime = 10000;
+
+void setup()
+{
+  pinMode(redLightPin, OUTPUT);
+  pinMode(yellowLightPin, OUTPUT);
+  pinMode(greenLightPin, OUTPUT);
+}
+
+void loop() 
+{
+  redLight();
+  greenLight();
+  yellowLight();
+}
+
+void redLight() {  
+  resetLights();
+  digitalWrite(redLightPin, HIGH);
+  delay(redTime);
+  
+}
+
+void yellowLight() {
+  int events = yellowTime / (yellowTimeInterval * 2);
+
+  resetLights();
+  for (int i = 1; i < events; i++) {
+    digitalWrite(yellowLightPin, HIGH);
+    delay(yellowTimeInterval);
+    digitalWrite(yellowLightPin, LOW);
+    delay(yellowTimeInterval);
+  }
+}
+
+void greenLight() {
+  resetLights();
+  digitalWrite(greenLightPin, HIGH);
+  delay(greenTime);
+}
+
+void resetLights() {
+  digitalWrite(redLightPin, LOW);
+  digitalWrite(yellowLightPin, LOW);
+  digitalWrite(greenLightPin, LOW);
+}
