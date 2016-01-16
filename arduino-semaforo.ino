b63bd9b78ca906893f465c9775609821eb3f6421
Ignacio Sánchez <chet161984@gmail.com>
2016-01-16 12:09:07 +0100

    Añadimos ls funcionalidad de luces para peatón

--- arduino-semaforo.ino
+++ arduino-semaforo.ino
@@ -2,17 +2,23 @@
 int redLightPin = 7;
 int yellowLightPin = 6;
 int greenLightPin = 5;
+int redPedestrianPin = 3;
+int greenPedestrianPin = 2;
 
 int redTime = 10000;
 int yellowTime = 4000;
 int yellowTimeInterval = 500;
 int greenTime = 10000;
+int pedestrianBlinkingTime = 3000;
+int pedestrianBlinkingInterval = 3000;
 
 void setup()
 {
   pinMode(redLightPin, OUTPUT);
   pinMode(yellowLightPin, OUTPUT);
   pinMode(greenLightPin, OUTPUT);
+  pinMode(redPedestrianPin, OUTPUT);
+  pinMode(greenPedestrianPin, OUTPUT);
 }
 
 void loop() 
@@ -25,14 +31,17 @@ void loop()
 void redLight() {  
   resetLights();
   digitalWrite(redLightPin, HIGH);
+  digitalWrite(greenPedestrianPin, HIGH);
   delay(redTime);
   
 }
 
 void yellowLight() {
-  int events = yellowTime / (yellowTimeInterval * 2);
-
   resetLights();
+  
+  digitalWrite(redPedestrianPin, HIGH);
+  
+  int events = yellowTime / (yellowTimeInterval * 2);
   for (int i = 1; i < events; i++) {
     digitalWrite(yellowLightPin, HIGH);
     delay(yellowTimeInterval);
@@ -43,6 +52,7 @@ void yellowLight() {
 
 void greenLight() {
   resetLights();
+  digitalWrite(redPedestrianPin, HIGH);
   digitalWrite(greenLightPin, HIGH);
   delay(greenTime);
 }
@@ -51,4 +61,6 @@ void resetLights() {
   digitalWrite(redLightPin, LOW);
   digitalWrite(yellowLightPin, LOW);
   digitalWrite(greenLightPin, LOW);
+  digitalWrite(redPedestrianPin, LOW);
+  digitalWrite(greenPedestrianPin, LOW);
 }
